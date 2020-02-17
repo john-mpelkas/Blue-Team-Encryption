896d02fb5f073a7092db7dcfb3bd7a1b40447a59
Mpelkas <mpelkasj@wit.edu>
2020-02-16 19:47:21 -0500

    Caesar cipher complete, needs notes as well as formating

--- Caesar Cipher/CaesarCipher.py
+++ Caesar Cipher/CaesarCipher.py
@@ -1,21 +1,47 @@
 
 
+# Function to handle ascii operations. This program will only encrypt characters
+# from the English alphet excluding special characters. Once the ascii value
+# exceeds 122 it will wrap back around to the begining of the ascii table
+def asciiWrapper(char, key):
 
-# def asciiWrapper(char):
-#     tempVal = ord(char)
-#     tempVal += 1
-#     #print (tempVal)
-#     return chr(tempVal)
+    asciiVal = ord(char)
+    asciiVal += key
+
+    if(ord(char) < 97 or ord(char) > 122):
+        return char
+    if (asciiVal > 122):
+        asciiVal = (asciiVal%122)+96
+
+    return chr(asciiVal)
+
+def arrJoiner(textCharArr):
+    text =""
+    for element in textCharArr:
+        text += element
+    return text
 
 def caesarCipher(text, key):
 
-    print (list(text[2]))
-    list(text[2]) = 'z'
+    textCharArr = (list(text))
+    for element in range(0, len(text)):
+        textCharArr[element] = asciiWrapper(textCharArr[element], key)
+
+    return arrJoiner(textCharArr)
 
 def main():
-    test = "aabbcc dog"
-    key = 1
-    caesarCipher(test, key)
+    #test = "Ice cream (derived from earlier iced cream or cream ice) is a sweetened frozen food typically eaten as a snack or dessert. It may be made from dairy milk or cream and is flavored with a sweetener, either sugar or an alternative, and any spice, such as cocoa or vanilla. Colourings are usually added, in addition to stabilizers."
+    #test = "aAbbcc the dog"
+    key = 2
+
+    f = open("PlainText.txt", "r")
+    if(f.mode == 'r'):
+        contents =f.read()
+    f.close()
+
+    f = open("CipherText.txt", "w+")
+    f.write(caesarCipher(contents.lower(), key))
+    f.close()
 
 
 
