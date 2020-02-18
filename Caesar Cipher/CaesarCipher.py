b73846077f7035c9738e1a998a5e839543c6c835
Mpelkas <mpelkasj@wit.edu>
2020-02-18 15:41:00 -0500

    Adding RSA folder

--- Caesar Cipher/CaesarCipher.py
+++ Caesar Cipher/CaesarCipher.py
@@ -1,10 +1,4 @@
-
-
-# Function to handle ascii operations. This program will only encrypt characters
-# from the English alphet excluding special characters. Once the ascii value
-# exceeds 122 it will wrap back around to the begining of the ascii table
 def asciiWrapper(char, key):
-
     asciiVal = ord(char)
     asciiVal += key
 
@@ -12,38 +6,30 @@ def asciiWrapper(char, key):
         return char
     if (asciiVal > 122):
         asciiVal = (asciiVal%122)+96
-
     return chr(asciiVal)
 
 def arrJoiner(textCharArr):
-    text =""
+    text = ""
     for element in textCharArr:
         text += element
     return text
 
 def caesarCipher(text, key):
-
     textCharArr = (list(text))
     for element in range(0, len(text)):
         textCharArr[element] = asciiWrapper(textCharArr[element], key)
-
     return arrJoiner(textCharArr)
 
-def main():
-    #test = "Ice cream (derived from earlier iced cream or cream ice) is a sweetened frozen food typically eaten as a snack or dessert. It may be made from dairy milk or cream and is flavored with a sweetener, either sugar or an alternative, and any spice, such as cocoa or vanilla. Colourings are usually added, in addition to stabilizers."
-    #test = "aAbbcc the dog"
-    key = 2
 
-    f = open("PlainText.txt", "r")
-    if(f.mode == 'r'):
-        contents =f.read()
-    f.close()
+#example
 
-    f = open("CipherText.txt", "w+")
-    f.write(caesarCipher(contents.lower(), key))
-    f.close()
+key = 2
+f = open("PlainText.txt", "r")
 
+if(f.mode == 'r'):
+    contents = f.read()
+f.close()
 
-
-if __name__ == "__main__":
-    main()
+f = open("CipherText.txt", "w+")
+f.write(caesarCipher(contents.lower(), key))
+f.close()
---
+++ RSA/README.md
@@ -0,0 +1,5 @@
+<h1>RSA Encryption (Rivest-Shamir-Adleman)</h1>
+
+
+Euclid's proof prime numbers
+p & q = prime numbers
