59e356c53b574cdf3dfd5ec9217ab78306ecc466
painchaudk <painchaudk@wit.edu>
2020-03-20 21:54:43 -0400

    -added RC4 -wrote ksa and prga -fixed spacing

--- rc4/rc4.py
+++ rc4/rc4.py
@@ -1,22 +1,23 @@
-#KSA is key-scheduling algorithm
-#PRGA is pseudo-random generation algorithm
-#key is one byte
-#s is 256-bytes
-#t is temporary 256-byte vector
+# KSA is key-scheduling algorithm
+# PRGA is pseudo-random generation algorithm
+# key is one byte
+# s is 256-bytes
+# t is temporary 256-byte vector
 
 
+# sets s equal to values 0 to 255
 
 
-#sets s equal to values 0 to 255
 def ksa (key):
     s = list(range(0,255))
     j = 0
     for i in range(0 ,255):
         j = (j + s[i] + key[i% len(key)])% 256
-        #swaps s at i and s at j
+        # swaps s at i and s at j
         s[i], s[j] = s[j], s[i]
 
     return s
+
 
 def prga (s):
     i = 0
@@ -27,23 +28,17 @@ def prga (s):
 
         # swaps s at i and s at j
         s[i], s[j] = s[j], s[i]
-        #holds value of s at i plus s at j
+        # holds value of s at i plus s at j
         t = (s[i] + s[j])%256
         k = s[t]
 
         yield k
 
-#generates the keystream
+# generates the keystream
+
+
 def get_ks(key):
     s = ksa(key)
     return prga(s)
 
-
-def encrypt(key, plaintext):
-
-
-def decrypt(key, ciphertext):
-
-
-def main():
 
